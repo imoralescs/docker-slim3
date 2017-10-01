b6f70907df6d33083451e1fa21c35704a4615e67
imoralescs <israel@savvycard.com>
2017-10-01 18:51:49 -0400

    Organize

--- app/Middleware/RedirectIfUnauthenticated.php
+++ app/Middleware/RedirectIfUnauthenticated.php
@@ -1,13 +1,40 @@
-<?php 
+<?php
 
 namespace App\Middleware;
 
+// To used container
+use Interop\Container\ContainerInterface;
+
+use Slim\Interfaces\RouterInterface;
+
 class RedirectIfUnauthenticated
 {
-	public function __invoke($request, $response, $next) 
+	// Using ContainerInterface
+	/*
+	protected $c;
+
+	public function __construct(ContainerInterface $c)
+	{
+			$this->c = $c;
+	}
+	*/
+
+	// Using RouterInterface
+	protected $router;
+
+	public function __construct(RouterInterface $router)
+	{
+			$this->router = $router;
+	}
+
+	public function __invoke($request, $response, $next)
 	{
 		if(!isset($_SESSION['user_id'])){
-			$response = $response->withRedirect($container->router->pathFor('login'));
+			// Using ContainerInterface
+			//$response = $response->withRedirect($this->c->router->pathFor('login'));
+
+			// Using RouterInterface
+			$response = $response->withRedirect($this->router->pathFor('login'));
 		}
 
 		return $next($request, $response);
--- routes/web.php
+++ routes/web.php
@@ -11,55 +11,60 @@
 
 	// Passing data to view without database
 	/*
-	$app->get('/users', function($request, $response){
-		$userdetail = [
-			'username' => 'SteveO',
-			'name' => 'Steve OHaire',
-			'email' => 'steveohaire@gmail.com'
-		];
+	$app->get('/customers', function($request, $response){
+		$customers = array(
+			array(
+				"username" => "peterP_02",
+				"email" => "peterparker@mail.com"
+			),
+			array(
+				"username" => "clark_070",
+				"email" => "clarkkent@mail.com"
+			),
+		);
 
-		return $this->view->render($response, 'users.twig',[
-			'user' => 'Steve',
-			'userdetail' => $userdetail
+		return $this->view->render($response, 'customers.twig',[
+			'customers' => $customers,
 		]);
 	})->setName('customers.index');
 	*/
 
 	// Passing data to view with database
 	$app->get('/customers', function($request, $response){
-		$users = $this->db->query("SELECT * FROM users")->fetchAll(PDO::FETCH_ASSOC);
+		$customers = $this->db->query("SELECT * FROM users")->fetchAll(PDO::FETCH_ASSOC);
 
 		return $this->view->render($response, 'customers.twig',[
-			'users' => $users
+			'customers' => $customers
 		]);
 	})->setName('customers.index');
 
 	// Route parameters without database
 	/*
-	$app->get('/users/{id}', function($request, $response, $args){
+	$app->get('/customer/{id}', function($request, $response, $args){
 		// look user up in db
-		$user = [
+		$customer = [
 			'id' => $args['id'],
 			'username' => 'Steve OHaire'
 		];
 
-		return $this->view->render($response, 'user.twig', compact('user'));
+		return $this->view->render($response, 'customer.twig', compact('customer'));
 	});
 	*/
 
 	// Route parameters with database
 	$app->get('/customer/{id}', function($request, $response, $args){
-		$user = $this->db->prepare("SELECT * FROM users WHERE id = :id");
-		$user->execute([
+		$customer = $this->db->prepare("SELECT * FROM users WHERE id = :id");
+		$customer->execute([
 			'id' => $args['id']
 		]);
 
-		$user = $user->fetch(PDO::FETCH_OBJ);
+		$customer = $customer->fetch(PDO::FETCH_OBJ);
 		return $this->view->render($response, 'customer.twig', [
-			'user' => $user
+			'user' => $customer
 		]);
 	});
 
+	// Redirect and Post
 	$app->get('/contact', function($request, $response){
 		return $this->view->render($response, 'contact.twig');
 	})->setName('contact');
@@ -103,6 +108,7 @@
 	});
 	*/
 
+	// Route for controller with model
 	$app->group('/users', function(){
 		$this->get('', UserController::class . ':index');
 	});
@@ -126,12 +132,25 @@
 	};
 	*/
 
+	// Middleware
 	use App\Middleware\RedirectIfUnauthenticated;
 
-	$app->get('/topics', TopicController::class . ':index')->add($middleware);
-	$app->get('/topics/api', TopicController::class . ':api');
+	//$app->get('/topics', TopicController::class . ':index')->add($middleware);
+	//$app->get('/topics/api', TopicController::class . ':api');
 	//$app->get('/topics/{id}', TopicController::class . ':show')->add($authenticated)->setName('topics.show');
-	$app->get('/topics/{id}', TopicController::class . ':show')->add(new RedirectIfUnauthenticated)->setName('topics.show');
+
+	// Using ContainerInterface
+	// $app->get('/topics/{id}', TopicController::class . ':show')->add(new RedirectIfUnauthenticated($container))->setName('topics.show');
+
+	// Using RouterInterface
+	//$app->get('/topics/{id}', TopicController::class . ':show')->add(new RedirectIfUnauthenticated($container['router']))->setName('topics.show');
+
+	// Adding Middleware to group routes
+	$app->group('/topics', function(){
+		$this->get('', TopicController::class . ':index');
+		$this->get('/{id}', TopicController::class . ':show')->setName('topics.show');
+	})->add(new RedirectIfUnauthenticated($container['router']));
+
 	$app->get('/login', function(){
 		return 'Login';
 	})->setName('login');
